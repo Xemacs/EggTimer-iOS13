1aa5ba14ac2918b3aa634c49a56976d85f2952d4
james stark <james.starkg@gmail.com>
2023-01-28 16:31:04 +1100

    Use switch

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let softTime = 5;
+    let medTime = 7;
+    let hardTime = 12;
     
-
-
+    @IBAction func EggPressed(_ sender: UIButton) {
+        let eggTime = sender.currentTitle;
+        print(buttonEgg(name: eggTime!));
+    }
+    
+    func buttonEgg(name: String) -> Int {
+        var minutes = 5;
+        switch name {
+        case "Soft":
+            minutes = softTime;
+            break;
+        case "Medium":
+            minutes = medTime;
+            break;
+        default :
+            minutes = hardTime;
+        }
+        return minutes
+    }
 }
